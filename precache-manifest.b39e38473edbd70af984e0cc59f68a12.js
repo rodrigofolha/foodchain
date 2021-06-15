fa4ec082d4ddeb9bbe8b3934b5bc5519e19019a7
05710063452 <rodrigo.folha@reitoria.ifpe.edu.br>
2021-06-15 14:31:35 -0300

    Updates

---
+++ precache-manifest.b39e38473edbd70af984e0cc59f68a12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfcea46d4cd8026543ececb4e672a39",
+    "url": "/manager-fronted/index.html"
+  },
+  {
+    "revision": "e737b8beda4606c9f9ee",
+    "url": "/manager-fronted/static/js/2.791a2aeb.chunk.js"
+  },
+  {
+    "revision": "99c5679c5ae299c14b8d938668660c83",
+    "url": "/manager-fronted/static/js/2.791a2aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ea80f21c6d016b5ac5",
+    "url": "/manager-fronted/static/js/main.ef403f3d.chunk.js"
+  },
+  {
+    "revision": "c215f36dd26e75270355",
+    "url": "/manager-fronted/static/js/runtime-main.ef8ccfff.js"
+  },
+  {
+    "revision": "62a8fc13ba80eeead44652daeb7eee02",
+    "url": "/manager-fronted/static/media/BlockFood.62a8fc13.jpg"
+  },
+  {
+    "revision": "fd8a5e78bf2f186311482c34e0069bd3",
+    "url": "/manager-fronted/static/media/oval.fd8a5e78.svg"
+  },
+  {
+    "revision": "a50c05471287e03cc2622e25a75d6bf3",
+    "url": "/manager-fronted/static/media/thumbnail.a50c0547.jpeg"
+  }
+]);
