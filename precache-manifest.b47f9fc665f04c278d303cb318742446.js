891708b970c3c4513f6696dfd1905521a1e9bca7
05710063452 <rodrigo.folha@reitoria.ifpe.edu.br>
2021-06-15 14:38:39 -0300

    Updates

---
+++ precache-manifest.b47f9fc665f04c278d303cb318742446.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbc9e38d82e2a21cf19479f48a2dcca",
+    "url": "/pack-food/index.html"
+  },
+  {
+    "revision": "d353a5b09421d9d177bb",
+    "url": "/pack-food/static/js/2.bf33348a.chunk.js"
+  },
+  {
+    "revision": "99c5679c5ae299c14b8d938668660c83",
+    "url": "/pack-food/static/js/2.bf33348a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ea80f21c6d016b5ac5",
+    "url": "/pack-food/static/js/main.ef403f3d.chunk.js"
+  },
+  {
+    "revision": "92c401c22c017dae8562",
+    "url": "/pack-food/static/js/runtime-main.677424d1.js"
+  },
+  {
+    "revision": "62a8fc13ba80eeead44652daeb7eee02",
+    "url": "/pack-food/static/media/BlockFood.62a8fc13.jpg"
+  },
+  {
+    "revision": "fd8a5e78bf2f186311482c34e0069bd3",
+    "url": "/pack-food/static/media/oval.fd8a5e78.svg"
+  },
+  {
+    "revision": "a50c05471287e03cc2622e25a75d6bf3",
+    "url": "/pack-food/static/media/thumbnail.a50c0547.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manager-fronted/precache-manifest.b39e38473edbd70af984e0cc59f68a12.js"
+  "/pack-food/precache-manifest.b47f9fc665f04c278d303cb318742446.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manager-fronted/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pack-food/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
