bd90a09fdfcd43f465da0874b33eeda9d0beda35
Rodrigo Folha <vinigo2@gmail.com>
2021-11-27 21:19:03 -0300

    removing non sense code

--- manager-frontend/src/pages/Menu/index.js
+++ manager-frontend/src/pages/Menu/index.js
@@ -184,15 +184,6 @@ export default function Dashboard() {
     </div>
 
     <CreateItem />
-
-      <div className="menu-items">
-        <Item  id="1" title="Combo Mega stacker - 2.0" description="Free refil, Batata Media, Mega Stacker Duplo" price="27"/>
-        <Item  id="2" title="Big Tasty" description="Pinto haha xisde" price="12"/>
-        <Item  id="3" title="Canastra burges" description="Muito querijo com presunt" price="12"/>
-        <Item  id="4" title="Puta do bairro" description="Varias puta mec mec" price="12"/>
-        <Item  id="5" title="Senhor alberto X" description="X tudao do senhor alberoto" price="12"/>
-        <Item  id="6" title="Carlão de 4" description="Completao do carlao com queijo parmesa" price="12"/>
-      </div>
     </MenuContainer>
     </>
   )
